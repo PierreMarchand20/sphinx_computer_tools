4f39c62af1d789c2bd513b65a6c8995c81ff9a71
Pierre Marchand <pierre.jimthefrench@gmail.com>
2023-06-29 12:33:27 +0200

    add pyproject.toml and formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ComputerTools'
-copyright = '2022, Pierre Marchand'
-author = 'Pierre Marchand'
+project = "ComputerTools"
+copyright = "2022, Pierre Marchand"
+author = "Pierre Marchand"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,14 +31,14 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.asciinema',
-    'sphinxcontrib.rsvgconverter',
-    'sphinx_copybutton',
-    'sphinx_contributors'
+    "sphinxcontrib.asciinema",
+    "sphinxcontrib.rsvgconverter",
+    "sphinx_copybutton",
+    "sphinx_contributors",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -62,7 +62,7 @@ rst_epilog = """
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 html_title = "Computer tools"
 
@@ -75,31 +75,29 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_css_files = [
-    "css/custom.css"
-]
+html_css_files = ["css/custom.css"]
 
 sphinxcontrib_asciinema_defaults = {
     # 'theme': 'solarized-dark',
-    'preload': 1,
+    "preload": 1,
     # 'font-family': "monospace, 'MesloLGS NF'",
     # 'font-size': '25',
     # 'path': '_static/asciicast/'
 }
 
 # -- Options for LaTeC output -------------------------------------------------
-latex_engine = 'lualatex'
+latex_engine = "lualatex"
 latex_elements = {
-    'tableofcontents': r"",
-    'preamble': r'''
+    "tableofcontents": r"",
+    "preamble": r"""
 \expandafter\def\expandafter\LaTeX\expandafter{\expandafter\text\expandafter{\LaTeX}}
 
-''',
+""",
     # https://tex.stackexchange.com/questions/572212/substituting-fonts-for-emojis-in-lualatex
     # Reset defaults plus fallback for emojis
-    'fontpkg': r'''
+    "fontpkg": r"""
 \directlua{luaotfload.add_fallback
    ("emojifallback",
     {
@@ -116,5 +114,5 @@ latex_elements = {
 ]
 \setsansfont{FreeSerif}
 \setmonofont{FreeMono}
-'''
+""",
 }
